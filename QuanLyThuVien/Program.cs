6c2e8c63ca82683f6191b73e07c3fc2017ee1814
Anger000 <77683727+khue24112000@users.noreply.github.com>
2021-03-24 07:54:03 +0700

    fixed hello

--- QuanLyThuVien/Program.cs
+++ QuanLyThuVien/Program.cs
@@ -11,6 +11,8 @@ namespace QuanLyThuVien
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// hello
+       
         [STAThread]
         static void Main()
         {
